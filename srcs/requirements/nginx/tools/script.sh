3e604af1bc7c62ae1cd8763c2e508833ba9738ef
Nikita Androsov <androsov@Nikitas-MacBook-Air.local>
2024-07-15 00:41:58 +0200

    it works

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \ 
-	-keyout /etc/ssl/private/nginx-selfsigned.key \
-	-out /etc/ssl/certs/nginx-selfsigned.crt \
-	-subj "/C=CZ/L=PR/O=42/OU=cadet/CN=[nandroso.42.fr](http://nandroso.42.fr/)"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+	-subj "/C=CZ/L=PR/O=42/OU=cadet/CN=nandroso.42.fr"
 
 echo "
 server {
@@ -20,12 +20,12 @@ server {
 	root /var/www/html;
 	index index.html;
 
-	location ~ [^/]\\.php(/|$) {
-	try_files $uri =404;
-	fastcgi_pass wordpress:9000;
-	include fastcgi_params;
-	fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-	}
+	# location ~ [^/]\\.php(/|$) {
+	# try_files $uri $uri/ =404;
+	# fastcgi_pass wordpress:9000;
+	# include fastcgi_params;
+	# fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+	# }
 }" > /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
