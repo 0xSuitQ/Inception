a19194827980a4da81ed5b9aae30b5121d634ac9
Nikita Androsov <androsov@Nikitas-MacBook-Air.local>
2024-07-15 23:41:13 +0200

    wordpress seems set up

--- srcs/requirements/nginx/utils/script.sh
+++ srcs/requirements/nginx/utils/script.sh
@@ -20,12 +20,12 @@ server {
 	root /var/www/html;
 	index index.html;
 
-	# location ~ [^/]\\.php(/|$) {
-	# try_files $uri $uri/ =404;
-	# fastcgi_pass wordpress:9000;
-	# include fastcgi_params;
-	# fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-	# }
+	location ~ [^/]\\.php(/|$) {
+	try_files $uri $uri/ =404;
+	fastcgi_pass wordpress:9000;
+	include fastcgi_params;
+	fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+	}
 }" > /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
---
+++ srcs/requirements/wordpress/utils/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir -p /var/www/html
+cd /var/www/html
+rm -rf ./*
+
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+
+wp core download --allow-root
+
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+sed -i -r "s/database_name_here/$MYSQL_DB/"   wp-config.php
+sed -i -r "s/username_here/$MYSQL_USR/"  wp-config.php
+sed -i -r "s/password_here/$MYSQL_PWD/"    wp-config.php
+sed -i -r "s/localhost/$MYSQL_HOSTNAME/"    wp-config.php
+
+wp core install --url=$URL/ --title=$TITLE --admin_user=$ADMIN_USR --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL --skip-email --allow-root
+
+wp theme install neve --allow-root
+wp theme activate neve --allow-root
+
+mkdir /run/php
+
+/usr/sbin/php-fpm7.4 -F
